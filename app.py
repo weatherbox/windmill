469687e498f40b62f06e1eb8018ac7fdc4a31051
tattii <yuta.tatti@gmail.com>
2018-02-27 05:54:38 +0900

    add sounding

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = domovoi.Domovoi()
 slack_token = os.environ["SLACK_API_TOKEN"]
 slack = SlackClient(slack_token)
 
-for func in ["jma-xml_atomfeed", "grib2tiles_download_msm", "grib2tiles_msm", "grib2tiles_tile-json-msm", "amedas_scrape-geojson"]:
+for func in ["jma-xml_atomfeed", "grib2tiles_download_msm", "grib2tiles_msm", "grib2tiles_tile-json-msm", "amedas_scrape-geojson", "sounding_sounding"]:
     @app.cloudwatch_logs_sub_filter_handler(log_group_name="/aws/lambda/" + func, filter_pattern='Error')
     def monitor_cloudwatch_logs(event, context):
         toslack(event['logGroup'][12:], event['logEvents'][0]['message'])
